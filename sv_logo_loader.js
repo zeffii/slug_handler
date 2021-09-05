4e34cb8d8dca47de8ad143c656fc9810e20d3af1
Dealga McArdle <zeffii@users.noreply.github.com>
2021-09-05 17:41:40 +0200

    Update sv_logo_loader.js

--- sv_logo_loader.js
+++ sv_logo_loader.js
@@ -1,7 +1,7 @@
 var sv_logo;
  
 function setup() {
-  createCanvas(700, 700, WEBGL);
+  createCanvas(700, 900, WEBGL);
   sv_logo = loadModel('sv_logo.obj');
 }
 
@@ -9,7 +9,8 @@ function draw() {
   background(250);
 
   normalMaterial();
-  translate(240, 0, 0);
+  scale(20, 20, 20);
+  translate(140, 0, 0);
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
