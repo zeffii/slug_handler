6a8283c5daf1667cb0fda6c83acc6131c05d723f
Dealga McArdle <zeffii@users.noreply.github.com>
2021-09-05 17:43:16 +0200

    Update sv_logo_loader.js

--- sv_logo_loader.js
+++ sv_logo_loader.js
@@ -9,7 +9,7 @@ function draw() {
   background(250);
 
   normalMaterial();
-  scale(20, 20, 20);
+  //scale(20, 20, 20);
   translate(140, 0, 0);
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
