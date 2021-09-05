5d7127f7245ff70d205340609aa854c68e2753eb
Dealga McArdle <zeffii@users.noreply.github.com>
2021-09-05 21:55:55 +0200

    Add files via upload

--- sv_logo_loader.js
+++ sv_logo_loader.js
@@ -1,7 +1,7 @@
 var sv_logo;
  
 function setup() {
-  createCanvas(900, 600, WEBGL);
+  createCanvas(700, 700, WEBGL);
   sv_logo = loadModel('sv_logo.obj');
 }
 
@@ -9,8 +9,7 @@ function draw() {
   background(250);
 
   normalMaterial();
-  //scale(20, 20, 20);
-  translate(140, 0, 0);
+  translate(240, 0, 0);
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
